0844afd997ac4a6a91e40759367c1204606448f0
DossJey <71648853+DossJey@users.noreply.github.com>
2022-10-04 18:02:19 +0200

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+import java.util.stream.DoubleStream;
+
+public class Calculator {
+
+    static double add(double... operands) {
+        return DoubleStream.of(operands)
+                .sum();
+    }
+
+    static double multiply(double... operands) {
+        return DoubleStream.of(operands)
+                .reduce(1, (a, b) -> a * b);
+    }
+
+    static double divide(int x, int y){
+
+        int z = x/y;
+
+        return z;
+    }
+}
